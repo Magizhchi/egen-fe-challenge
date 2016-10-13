330460cf5f7ed56a8202f1a2fec8ce738c84b4c9
Karthikk <karthikk296d@gmail.com>
2016-10-13 15:45:04 -0500

    Component with dummy template works

--- public/src/users/app/users.component.js
+++ public/src/users/app/users.component.js
@@ -1,3 +1,14 @@
 /**
  * Created by Karthikk on 10/13/2016.
  */
+
+(function (app) {
+    app.UsersComponent = ng.core
+        .Component({
+            selector: 'egen',
+            template: '<h1>Hello from Component</h1>'
+        })
+        .Class({
+            constructor: function () {}
+        });
+}) (window.app || (window.app = {}));
