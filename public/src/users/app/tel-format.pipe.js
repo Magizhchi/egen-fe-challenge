dfe260851ceb4b35bbce6fd950774e2940d3f8d8
Karthikk <karthikk296d@gmail.com>
2016-10-14 17:59:10 -0500

    Added Pakistan And Belgiu Formats

--- public/src/users/app/tel-format.pipe.js
+++ public/src/users/app/tel-format.pipe.js
@@ -7,9 +7,13 @@
             constructor: function() {},
             transform: function (phone) {
                 if (phone.startsWith("+1")) // USA
-                    return phone.replace(/(\d{1})(\d{3})(\d{3})(\d{4})/, '$1($2)-$3-$4');
+                    return phone.replace(/(\d{1})(\d{3})(\d{3})(\d{4})/, '$1($2) $3-$4');
                 else if (phone.startsWith("+91")) // India
                     return phone.replace(/(\d{2})(\d{5})(\d{5})/, '($1) $2-$3');
+                else if (phone.startsWith("+32")) // Belgium
+                    return phone.replace(/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/, '($1) $2 $3 $4 $5');
+                else if (phone.startsWith("+03")) // Pakistan
+                    return phone.replace(/(\d{2})(\d{2})(\d{7})/, '($1) $2-$3');
                 else
                     return phone ;
             }
--- test/client/users/tel-format.pipe-test.js
+++ test/client/users/tel-format.pipe-test.js
@@ -11,4 +11,18 @@ describe('tel-format pipe test', function () {
             Reflect.getMetadata('annotations', app.TelFormatPipe)[0];
         expect(annotations.name).to.be.eql('telFormat');
     });
+
+    it('phone numbers starting with +1 should be formatted to USA', function () {
+        expect(formatPipe.transform('+11234567890')).to.be.eql('+1(123) 456-7890');
+    });
+    it('phone numbers starting with +91 should be formatted to India', function () {
+        expect(formatPipe.transform('+911234567890')).to.be.eql('+(91) 12345-67890');
+    });
+    it('phone numbers starting with +32 should be formatted to Belgium', function () {
+        expect(formatPipe.transform('+3212345678')).to.be.eql('+(32) 12 34 56 78');
+    });
+    it('phone numbers starting with +03 should be formatted to Belgium', function () {
+        expect(formatPipe.transform('+03012345678')).to.be.eql('+(03) 01-2345678');
+    });
+
 });
