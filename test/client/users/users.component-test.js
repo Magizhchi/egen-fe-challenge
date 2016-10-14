a1623016d0e664095f4162677bdc70dc2a309cf0
Karthikk <karthikk296d@gmail.com>
2016-10-14 16:20:53 -0500

    Checking In Component - Class tests

--- test/client/users/users.component-test.js
+++ test/client/users/users.component-test.js
@@ -1,4 +1,24 @@
 describe('user component tests', function () {
     it('Canary Test', function () {
     });
+
+    it('component should set the selector Attribute', function () {
+       var componentAnnotations = Reflect.getMetadata('annotations', app.UsersComponent)[0];
+
+        expect(componentAnnotations.selector).to.be.eql('egen');
+    });
+
+    it('component should set the templateUrl Attribute', function () {
+        var componentAnnotations = Reflect.getMetadata('annotations', app.UsersComponent)[0];
+
+        expect(componentAnnotations.templateUrl).to.be.eql('src/users/users.html');
+    });
+
+    it('should initialize errorMessage to an empty string', function() {
+        expect(use.message).to.be.eql('');
+    });
+
+    // it('should initialize people to an empty array', function() {
+    //     expect(peopleComponent.people).to.be.eql([]);
+    // });
 })
